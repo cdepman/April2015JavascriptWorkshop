c15d90448b327bead8e8ad3fdcea28b18abe7a54
Charlie Depman <cdepman@gmail.com>
2015-04-13 19:43:54 +0300

    initial commit

---
+++ objectExercises/index.js
@@ -0,0 +1,45 @@
+var medicalRobot = {
+
+  name: 'eR42',
+
+  model: 'Basic Medical Bot',
+
+  homeHospital: 'Nairobi General Hospital (-1.296276, 36.804510)',
+
+  instructionSet: medRobotInstructionSetV14,
+
+  currentLocation: function(){
+    return '';
+  },
+
+  heal: function(bodypart){
+    if (this.currentLocation() !== this.homeHospital){
+      return "Operation aborted. Return " + this.name + " to " + this.homeHospital + '.';
+    }
+    var assessment = this.instructionSet.assess(bodypart);
+    console.log('Assessment: \n' + "Patient's " + bodypart + ' is ' + assessment + '.');
+    var isHealed = this.instructionSet.performHeal(assessment);
+    if (isHealed){
+      return "Patient's " + assessment + ' ' + bodypart + ' is healed.';
+    } else {
+      return "Healing failed. Patient's " + bodypart + ' is still ' + assessment + '. Seek specialized treatment.';
+    }
+  },
+
+  greet: function(patientName){
+    return "Hello, " + patientName + " I'm " + this.name + ".";
+  }
+};
+
+var medRobotInstructionSetV14 = {
+  assess: function(bodypart){
+    var conditions = ['sprained', 'broken', 'cut', 'punctured','missing', 'bruised', 'infected'];
+    return conditions[Math.round(Math.random()* (conditions.length - 1))];
+  },  
+  performHeal: function(assessment){
+    if (assessment === 'missing'){
+      return false;
+    }
+    return Math.random() > 0.5 ? true : false;
+  }
+};
