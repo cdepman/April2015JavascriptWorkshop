17a3969f658285ed6fd7e5ce216275f97f3186ce
Charlie Depman <cdepman@gmail.com>
2015-04-16 19:44:19 +0300

    adds conditionalExercises

---
+++ conditionalExercises/index.js
@@ -0,0 +1,12 @@
+var checkID = function(age){
+
+  if (/* Your Code Here*/){
+    console.log('ID holder is underage. Do not admit!');
+  } else if (/* Your Code Here*/){
+    console.log('ID holder is over 60. Give senior discount!');
+  } else {
+    console.log('ID holder may enter!')
+  }
+
+}
+
